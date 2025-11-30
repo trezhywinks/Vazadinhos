3248e28f9659cec839e7891ad7e58a3a05117adc
Trezhy Phantom <dreqxyonion@gmail.com>
2025-11-30 13:11:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.button').addEventListener('click', () =>{
+  document.querySelector('.server').style.display = "none";
+  document.querySelector('.pag').style.display = "block";
+});
